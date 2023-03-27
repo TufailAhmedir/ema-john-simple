bd5657981e07f2a7e32285e9a923cdbd421681ef
TufailAhmedir <tufailahmed1589@gmail.com>
2023-03-27 12:52:40 +0600

    third commit

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
 import'./Product.css';
 const Product = (props) => {
+    // console.log(props);
     const {img, name, seller, quantity, price} = props.product;
+    const handleAddToCart = props.handleAddToCart;
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -11,7 +16,9 @@ const Product = (props) => {
             <p>Manufacturer: {seller}</p>
             {/* <p>Ratings:{ratings}</p> */}
             </div>    
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={() =>{handleAddToCart (props.product)}} className='btn-cart'>Add to Cart
+            <FontAwesomeIcon icon={faShoppingCart} />
+            </button>
         </div>
     );
 };
